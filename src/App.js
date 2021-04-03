f32c46f8c1cabe750c1a4c8a41e0462cddf500b0
Aditya Vartak <adityavartak1001@gmail.com>
2021-04-04 00:32:55 +0530

    calc app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {Button} from 'reactstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state={
+      currentValue:''
+    }
+    this.handleClick=this.handleClick.bind(this)
+    this.delete=this.delete.bind(this)
+    this.evaluate=this.evaluate.bind(this)
+
+
+  }
+  handleClick(e){
+    e.preventDefault()
+    this.setState({
+      
+     currentValue:this.state.currentValue+e.target.value
+    })
+  }
+  delete(e){
+    e.preventDefault()
+    this.setState({
+      
+     currentValue:''
+    })
+  }
+
+
+  evaluate(){
+    var x=eval(this.state.currentValue);
+    this.setState({
+      currentValue:x.toFixed(4)
+    })
+  }
+  
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        
+        <div id="numbers" className="col-md-12 ">
+          <div className="display"  >
+          {this.state.currentValue}
+          </div>
+          <div className="buttons">
+          <div className="grid" >
+          <Button value="1" onClick={this.handleClick}>1</Button>
+          <Button value="2"  onClick={this.handleClick}>2</Button>
+          <Button value="3"  onClick={this.handleClick}>3</Button>
+          <Button id="operator" value="/"  onClick={this.handleClick}>/</Button>
+          <Button onClick={this.delete} id="operator">AC</Button>
+
+        </div>
+        <div className="grid">
+          <Button value="4"  onClick={this.handleClick}>4</Button>
+          <Button value="5"  onClick={this.handleClick}>5</Button>
+          <Button value="6"  onClick={this.handleClick}>6</Button>
+          <Button id="operator" value="*"  onClick={this.handleClick}>*</Button>
+
+        </div>
+        <div className="grid">
+          <Button value="7"  onClick={this.handleClick}>7</Button>
+          <Button value="8"  onClick={this.handleClick}>8</Button>
+          <Button value="9"  onClick={this.handleClick}>9</Button>
+          <Button id="operator" value="+"  onClick={this.handleClick}>+</Button>
+
+        </div>
+        <div className="grid">
+          <Button value="0"  onClick={this.handleClick}>0</Button>
+          <Button value="."  onClick={this.handleClick}>.</Button>
+          <Button value=".0"  onClick={this.handleClick}>.0</Button>
+          <Button id="operator" value="-"  onClick={this.handleClick}>-</Button>
+          
+          <Button  id="operator" value="=" onClick={this.evaluate}>=</Button>
+
+        </div>
+        
+          
+          
+          </div>
+         
+
+
+
+
+
+        </div>
+        
+
+      </div>
+    );
+  }
 }
 
 export default App;
